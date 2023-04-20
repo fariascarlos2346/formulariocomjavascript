f223a0bf37981179362f6529b354837be894253e
Carlos Farias <fariascarlos2346@gmail.com>
2023-04-20 14:25:40 -0300

    alterações no js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const password = document.querySelector("#password");
+const jobSelect = document.querySelector("#job");
+const messageTextarea = document.querySelector("#message");
